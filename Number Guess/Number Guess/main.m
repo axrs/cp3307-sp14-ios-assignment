683ac634a576b0db02922a094f9a55b50dcb278c
Alexander Scott <xander@Alexanders-Mac.local>
2014-05-22 09:29:36 +1000

    XCode Init

---
+++ Number Guess/Number Guess/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Number Guess
+//
+//  Created by Alexander Scott on 22/05/2014.
+//  Copyright (c) 2014 ITJCU Mobile Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
