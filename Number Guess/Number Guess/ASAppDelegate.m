26a640f42bf5783c1aa9d0a03a0bd2b9b2d9e387
Alexander Scott <xander@Alexanders-Mac.local>
2014-06-04 21:44:48 +1000

    Background Audio

--- Number Guess/Number Guess/ASAppDelegate.m
+++ Number Guess/Number Guess/ASAppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "ASAppDelegate.h"
-
+#import "ASAudioEngine.h"
 @implementation ASAppDelegate
 
 @synthesize managedObjectContext = _managedObjectContext;
@@ -16,21 +16,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [self.window setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png"]]];
+    [ASAudioEngine startBackgroundAudio];
+
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [ASAudioEngine stopBackgroundAudio];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [ASAudioEngine startBackgroundAudio];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
