3f3df4a877d567253f83dcbc72e79b714361cbc5
Alexander Scott <xander@cadcoder.com>
2015-04-14 18:05:18 +1000

    Root Nav Controller restored

---
+++ Number Guess/ASRootNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ASRootNavigationController.h
+//  Number Guess
+//
+//  Created by Alexander Scott on 2/06/2014.
+//  Copyright (c) 2014 ITJCU Mobile Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASRootNavigationController : UINavigationController <UINavigationControllerDelegate>
+@property(nonatomic,assign) BOOL isInGame;
+@end
